04043b2adffed6873cefaacbb34e28000730158d
Ancelotow <ancelotow@gmail.com>
2022-09-26 17:47:34 +0200

    .feat: validator SS number

--- src/main/java/fr/esgi/cleancode/service/DrivingLicenceValidatorService.java
+++ src/main/java/fr/esgi/cleancode/service/DrivingLicenceValidatorService.java
@@ -1,11 +1,26 @@
 package fr.esgi.cleancode.service;
 
+import fr.esgi.cleancode.exception.InvalidDriverSocialSecurityNumberException;
 import fr.esgi.cleancode.model.DrivingLicence;
+
+import java.util.regex.Pattern;
 
 public class DrivingLicenceValidatorService {
 
-    public boolean isValidSSNumber(DrivingLicence drivingLicence) {
-
+    public boolean isValidSSNumber(DrivingLicence drivingLicence) throws InvalidDriverSocialSecurityNumberException {
+        String securitySocialNumber = drivingLicence.getDriverSocialSecurityNumber();
+        if(securitySocialNumber == null) {
+            throw new InvalidDriverSocialSecurityNumberException("Le numéro de sécurité social est null");
+        } else if(securitySocialNumber.isEmpty()) {
+            throw new InvalidDriverSocialSecurityNumberException("Le numéro de sécurité social est vide");
+        } else if(securitySocialNumber.length() != 15) {
+            throw new InvalidDriverSocialSecurityNumberException("Le numéro de sécurité social doit contenir 15 caractères");
+        } else if (!Pattern.matches("\\d*", securitySocialNumber)) {
+            throw new InvalidDriverSocialSecurityNumberException("Le numéro de sécurité social doit contenir uniquement des chiffres");
+        } else {
+            return true;
+        }
     }
+
 
 }
